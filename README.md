444de8bde0ad9e985864dd5ab42be2baabf04317
Gerisi <gerisikola0@gmail.com>
2024-11-27 17:51:45 +0100

    009 J'ai refait l'agrandissement de la grille en utillisant numpy

--- README.md
+++ README.md
@@ -9,20 +9,28 @@
     Si on a appuiyer on met les generation en pose le temps d'ajouter des cases au tableau.
 - Pour que les **calcul reste correcte** on peut par exemple cree 5 case au debut et ajouter +5 a tout les position pour les garder inchanger.
 - Pour **otimiser les calcul**, au lieu de parcourir une liste casi vide gigantesque. On pourrais garder en memoire toutes les positions des cellule vivante leurs voisines.
+- Pour que l'utilisateur ne puisse **pas** mettre des **cellules aux bores**, on peut faire que la grille visuel (GUI) soit plus petites que la grille réel.
 
 ### Jounal
-- ***20-11*** : Claude m'a pendu 4 GUI fait avec tkinter / kivy / Qt / pygame. Je vais d'abord me pencher sur l'algo.
-- J'ai une fonction qui trouve les cellule vivantes et une qui calcule les voisines et met les points des positions en memoires
+- ***20-11*** : Claude m'a pendu 4 GUI fait avec tkinter / kivy / Qt / pygame. Je vais d'abord me pencher sur **l'algo**.
+- J'ai une fonction qui trouve les **cellule vivantes** et une qui calcule **les voisines** et met les points des **positions en memoires**
+
 - ***21-11*** : La fonction qui calcul les voisinages crash si il y a une cellule à la dernière ligne. (Problème N1). J'ai changer le code pour être en POO. J'ai fait une méthode pour agrandir "l'aréne" mais il y a un problème (F1)
+
 - ***24-11*** : J'ai créé une version json du fichier des constates et j'ai aussi créé un scripte pour les récupérer
+
 - ***25-11*** : En classe : le prof m'a dit de tester un GUI avec matplotlib, ça marche et on peut l'intégré dans une fenétre tkinter. J'ai fait un grillage et 2 boutons (agrandir et retrecir la grille)
+
 - ***26-11*** : J'ai fais quelques test sur les GUI matplotlib (J'ai oublié lque j'avais déjà un script pour window alors je l'ai refait)
+
+- ***27-11*** : J'ai refait **l'agrandissement de la grille**, au lieu d'agrandir la liste j'en créé une nouvelle a chaque extention en utilisant **numpy**. Et j'ai **supprimer les GUI exemple** (kivy, pygame,...)
+
 
 ### Problèmes
 #### Not fix
 1. La Méthode qui calcul les voisinages crash à du mal à gérer les cellules des bors, de tous les coté.
 
 #### Fix
-1. Aprés la 2 extention du territoire, il y a un bug et des cellules meur. à fixer
+1. Aprés la 2 extention du territoire, il y a un bug et des cellules meur. à fixer (27-11 : Le probleme est résolu. Au lieu d'agrandir la liste j'en créé une nouvelle a chaque extention en utilisant numpy)
 
 
--- algo.py
+++ algo.py
@@ -15,17 +15,37 @@ stase : 12 et 13
 né : 3 !!!
 """
 
+import numpy as np
+
 class AlgoGameOfLife():
     def __init__(self):
-        self.cell_in_life = []
-        self.cell_edge=[[0,0,0,0,0,0,0],
+        
+        self.a = 7
+        
+        self.cell_in_life = [[1,3],[2,2],[3,2],[3,3],[3,4]]  #[[3,2],[3,3],[3,4]]
+        self.cell_edge = np.zeros((self.a, self.a), dtype=int) 
+        self.plus_plus()
+        """self.cell_edge=[[0,0,0,0,0,0],
+                        [0,0,0,0,0,0],
+                        [0,0,0,0,0,0],
+                        [0,0,0,0,0,0],
+                        [0,0,0,0,0,0],
+                        [0,0,0,0,0,0]]"""
+        """[[0,0,0,0,0,0,0],
+                        [0,0,10,0,0,0,0],
+                        [0,10,0,0,0,0,0],
+                        [0,10,10,10,0,0,0],
+                        [0,0,0,0,0,0,0],
+                        [0,0,0,0,0,0,0],
+                        [0,0,0,0,0,0,0]]"""
+        """[[0,0,0,0,0,0,0],
                         [0,0,0,0,0,0,0],
                         [0,0,0,10,0,0,0],
                         [0,0,10,0,0,0,0],
                         [0,0,10,10,10,0,0],
                         [0,0,0,0,0,0,0],
                         [0,0,0,0,0,0,0]]
-        
+        """
         
         """self.space =   [[0,0,0,0,0,0],
                         [0,0,0,0,0,0],
@@ -52,7 +72,9 @@ class AlgoGameOfLife():
             for j in range(len(self.cell_edge[i])):
                 if self.cell_edge[i][j] >= 10:
                     #print(i,j)
-                    self.cell_in_life.append([i,j])
+                    self.cell_in_life.aself.cell_edgeend([i,j])
+    
+    
     
     def cell_edge_calcul(self):
         """Calcul les voisinages"""
@@ -61,29 +83,27 @@ class AlgoGameOfLife():
             
             #on verifie la position des ranger pour de pas sortir de l'index
             #      ___     ___    ___     ___ Problème 1  ___     ___       ___     ___ 
-            if i > 0:
+            """if i > 1:
                 i_range = (i-1,i+2)
             elif i < len(self.cell_in_life): # < ou <= ?? ------   Pk  self.cell_in_life ?????????????????????????????????????????????????????????????????????????
                 i_range = (i+1,i+2)
             else:
-                i_range = (i-1,i+1)
+                i_range = (i-1,i+1)"""
             #i_range = (i-1,i+2)
             
             # on fait le calcul des valeurs voisine
-            for it in range(i_range[0],i_range[1]):
-                if j-1 != -1:
+            if i > 0:
+                for it in range(i-1,i+2):#i_range[0],i_range[1]):
+                    #if j-1 != -1:
                     self.cell_edge[it][j-1] += 1
-                
-                if it != i:
-                    self.cell_edge[it][j] += 1
-                try:
-                    self.cell_edge[it][j+1] += 1
-                except:
-                    print("exhec")
-                    pass
-        
-        
-        
+                    
+                    if it != i:
+                        self.cell_edge[it][j] += 1
+                    try:
+                        self.cell_edge[it][j+1] += 1
+                    except:
+                        print("exhec")
+                        pass
         
         print("\ncell_edge")
         for i in range(len(self.cell_edge)):
@@ -93,6 +113,8 @@ class AlgoGameOfLife():
     
     def born_and_die(self):
         """Procéde aux naissances et aux morts"""
+        
+        self.cell_in_life = []
         for i in range(len(self.cell_edge)):
             for j in range(len(self.cell_edge)):
                 # Sur/sous population = mort (1 à 2 / 4 à 11 / +14)
@@ -106,26 +128,28 @@ class AlgoGameOfLife():
                 # Survie/naissance (3 /12 / 13)
                 elif 3 == self.cell_edge[i][j] or 12 <= self.cell_edge[i][j] <=13:
                     self.cell_edge[i][j] = 10
+                    self.cell_in_life.append([i,j])
         
+        print(self.cell_in_life)
         print("\nborn and die")
         for i in range(len(self.cell_edge)):
             print(self.cell_edge[i])
-        print(f"{len(self.cell_edge)}")
+        print(f"{len(self.cell_edge)}x{len(self.cell_edge[0])}")
     
     
     def check_rim(self):
-        """Verifiesi les cell s'approche des bores de <<l'arene>>"""
+        """Verifiesi les cell s'aself.cell_edgeroche des bores de <<l'arene>>"""
         col_right = len(self.cell_edge) -2
         col_left = 1
         raw_top = 1
-        raw_down = len(self.cell_edge[0]) -2
+        raw_down = len(self.cell_edge) -2
         rim = False
         
         """print(self.cell_in_life)
         print(len(self.cell_in_life))"""
         
-        for i in range(1,len(self.cell_in_life)):
-            for j in range(1,len(self.cell_in_life[0])):
+        for i in range(0,len(self.cell_in_life)):
+            for j in range(0,len(self.cell_in_life[0])):
                 if self.cell_in_life[i][j] == raw_down or\
                             self.cell_in_life[i][j] == raw_top or\
                             self.cell_in_life[i][j] == col_left or\
@@ -133,7 +157,7 @@ class AlgoGameOfLife():
                     rim = True
         if rim:
             self.protection_from_rim()
-            self.cell_in_life_correction()
+            #self.cell_in_life_correction()
                     #pass
     
     
@@ -146,29 +170,68 @@ class AlgoGameOfLife():
         # on copie la liste pour pouvoir la modifier dans la boucle
         cell_in_life_copy = self.cell_in_life.copy()
         for i in range(len(self.cell_in_life)):
-                cell_in_life_copy[i][0] += 1
+                #cell_in_life_copy[i][0] += 1
                 cell_in_life_copy[i][1] += 1
         self.cell_in_life = cell_in_life_copy
     
     
-    
+    """def vertical(self):
+        len_list = [0 for i in range(len(self.cell_edge[0]))]
+        self.cell_edge.insert(0,len_list)
+        self.cell_edge.aself.cell_edgeend(len_list)
+        
+    def horizon(self):
+        cell_edge_copy = self.cell_edge.copy()
+        for i in range(len(cell_edge_copy)):
+            self.cell_edge[i].insert(0,self.a)
+            #self.cell_edge[i].aself.cell_edgeend(self.a)
+            self.a += 1"""
     
     def protection_from_rim(self):
         """Ajoute une colone en haut et en bas 
             + Ajoute une ligne à gauche et à droite """
         
-        len_i = len(self.cell_edge[0])+2
-        len_list = [0 for i in range(len_i)]
+        #self.vertical()
+        #self.horizon()
+        
+        self.a += 1
+        
+        self.cell_edge = np.zeros((self.a, self.a), dtype=int) 
+        self.cell_in_life_correction()
+        self.plus_plus()
+        
+        print("\nadd")
+        for i in range(len(self.cell_edge)):
+            print(self.cell_edge[i])
+        print(f"{len(self.cell_edge)}x{len(self.cell_edge[0])}")
+        print("")
+        
+        
+        """len_i = len(self.cell_edge)
+        
         # on copie la liste pour pouvoir la modifier dans la boucle
+        
+        
+        
+        
+        
+        
+        len_list = [0 for i in range(len(self.cell_edge[0]))]
+        print(f"len(self.cell_edge) = {len(self.cell_edge)} \nlen_list = {len(len_list)}")
+        
         cell_edge_copy = self.cell_edge.copy()
         
         
-        for i in range(len(cell_edge_copy)):
-            self.cell_edge[i].insert(0,0)
-            self.cell_edge[i].append(0)
-        
         self.cell_edge.insert(0,len_list)
-        self.cell_edge.append(len_list)
+        self.cell_edge.aself.cell_edgeend(len_list)
+        
+        for i in range(len(cell_edge_copy)):
+            self.cell_edge[i].insert(0,self.a)
+            #self.cell_edge[i].aself.cell_edgeend(self.a)
+            self.a += 1
+        
+        
+        print(len(self.cell_edge),len(self.cell_edge[0]))"""
         
         """print("\nadd")
         for i in range(len(self.cell_edge)):
@@ -177,117 +240,28 @@ class AlgoGameOfLife():
     
     
     
-    def first_cell_borne(self):
-        """[inuttile] Ajoute 9 pour regle des probleme au debut"""
-        for item in self.cell_in_life:
-            #Ca c'est pour que la celule vivante soit à 10 après les calculs
-            self.cell_edge[item[0]][item[1]] += 9
+    def plus_plus(self):
+        for i in self.cell_in_life:
+            self.cell_edge[i[0]][i[1]] = 10
+    
+    
+    def generation_manager(self, num_of_gen=1):
+        #self.scan_space()
+        for _ in range(num_of_gen):
+            
+            #print(self.cell_in_life)
+            self.cell_edge_calcul() # calcul le voisinage
+            self.born_and_die() # defini les celules vivantes et mortes
+            self.check_rim() # verifie si il y a des celules qui s'aself.cell_edgeroche du bore
+            #print(" ------ new gen ---------")
+            #self.protection_from_rim()
+            """for i in range(len(self.cell_edge)):
+                print(self.cell_edge[i])
+            print("")"""
+            
+        
 
 
 if __name__ == "__main__":
     g = AlgoGameOfLife()
-    """g.scan_space()
-    g.protection_from_rim()"""
-    g.scan_space()
-    #g.first_cell_borne()
-    g.cell_edge_calcul()
-    g.born_and_die()
-    g.check_rim()
-    
-    g.scan_space()
-    #g.first_cell_borne()
-    g.cell_edge_calcul()
-    g.born_and_die()
-    g.check_rim()
-    
-    g.scan_space()
-    #g.first_cell_borne()
-    g.cell_edge_calcul()
-    g.born_and_die()
-    g.check_rim()
-    
-    g.scan_space()
-    #g.first_cell_borne()
-    g.cell_edge_calcul()
-    g.born_and_die()
-    g.check_rim()
-    
-    g.scan_space()
-    #g.first_cell_borne()
-    g.cell_edge_calcul()
-    g.born_and_die()
-    g.check_rim()
-    
-    g.scan_space()
-    #g.first_cell_borne()
-    g.cell_edge_calcul()
-    g.born_and_die()
-    g.check_rim()
-    
-    
-    g.scan_space()
-    #g.first_cell_borne()
-    g.cell_edge_calcul()
-    g.born_and_die()
-    g.check_rim()
-    
-    g.scan_space()
-    #g.first_cell_borne()
-    g.cell_edge_calcul()
-    g.born_and_die()
-    g.check_rim()
-    
-    g.scan_space()
-    #g.first_cell_borne()
-    g.cell_edge_calcul()
-    g.born_and_die()
-    g.check_rim()
-    
-    g.scan_space()
-    #g.first_cell_borne()
-    g.cell_edge_calcul()
-    g.born_and_die()
-    g.check_rim()
-    
-    g.scan_space()
-    #g.first_cell_borne()
-    g.cell_edge_calcul()
-    g.born_and_die()
-    g.check_rim()
-    
-    g.scan_space()
-    #g.first_cell_borne()
-    g.cell_edge_calcul()
-    g.born_and_die()
-    g.check_rim()
-    
-    g.scan_space()
-    #g.first_cell_borne()
-    g.cell_edge_calcul()
-    g.born_and_die()
-    g.check_rim()
-    
-    g.scan_space()
-    #g.first_cell_borne()
-    g.cell_edge_calcul()
-    g.born_and_die()
-    g.check_rim()
-    
-    g.scan_space()
-    #g.first_cell_borne()
-    g.cell_edge_calcul()
-    g.born_and_die()
-    g.check_rim()
-    
-    g.scan_space()
-    #g.first_cell_borne()
-    g.cell_edge_calcul()
-    g.born_and_die()
-    g.check_rim()
-    
-    
-    """g.scan_space()
-    g.cell_edge_calcul()
-    g.born_and_die()"""
-    #g.protection_from_rim()
-    
+    g.generation_manager(5)
